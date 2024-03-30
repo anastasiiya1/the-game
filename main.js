b13b0528726a59e28a854eaf6784f2dcec180561
anastasiiya1 <shapoval.anastasiiya@gmail.com>
2024-03-30 18:21:57 +0200

    draft 1

---
+++ main.js
@@ -0,0 +1,53 @@
+const startBtn = document.querySelector(".start-btn");
+const container = document.querySelector(".container");
+
+startBtn.addEventListener("click", handleStart);
+
+function createMarkup() {
+  container.innerHTML = `
+    <li class ="container-child"> </li>
+    <li class ="container-child"> </li>
+    <li class ="container-child"> </li>
+    <li class ="container-child"> </li>
+    <li class ="container-child"> </li>`;
+}
+
+function handleStart() {
+  createMarkup();
+  startBtn.disabled = true;
+
+  const promises = [...container.children].map(() => {
+    return new Promise((resolve, reject) => {
+      const random = Math.random();
+
+      if (random < 0.5) {
+        resolve("🤑");
+      } else {
+        reject("😈");
+      }
+    });
+  });
+  Promise.allSettled(promises).then((value) => {
+    value.forEach((item, i) => {
+      container.children[i].textContent = "";
+
+      setTimeout(() => {
+        container.children[i].textContent = item.value || item.reason;
+        const winner = value.every((item) => item.status === "fulfilled");
+        const looser = value.every((item) => item.status === "rejected");
+
+        if (i === value.length - 1) {
+          const instance = basicLightbox.create(`
+          <div class="modal">
+          <p>
+          ${winner ? "🥳 Congratulations! You won!" : looser ? "💩 You'll be lucky next time...maybe" : "🎲 Try one more time!"}
+          </p>
+          </div>`);
+          instance.show();
+          
+          startBtn.disabled = false;
+        }
+      }, 1000 * (i + 1));
+    });
+  });
+}
